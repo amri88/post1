6a17b421549c204f55d3f4c0233999508d2989fe
amri88 <32216486+amri88@users.noreply.github.com>
2017-09-23 18:30:16 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,7 +26,7 @@ if(x=="1"){xxx="/2017/09/money-saving-travel-tips.html"}
 if(x=="2"){xxx="/2017/09/wonderfully-useful-blogging-tips-for.html"}
 if(x=="3"){xxx="/2017/09/practical-and-useful-tips-for-traveling.html"}
 if(x=="4"){xxx="/2017/09/insurance.html"}
-if(x=="5"){xxx="/2017/09/how-to-lose-weight-in-week.html
+if(x=="5"){xxx="/2017/09/how-to-lose-weight-in-week.html"}
 if(x=="6"){xxx="/2017/09/what-is-forex-trading.html"}a_to_ve[i].href="https://loncat-dulu.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
